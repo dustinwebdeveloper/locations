7e0b38a2b62526ab0e72155e07cb150dec1a3025
dustinwebdeveloper <dustinwebdeveloper@gmail.com>
2021-05-22 19:49:19 +0200

    changed locations to app

--- hello/views.py
+++ hello/views.py
@@ -5,7 +5,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Locations!')
+    return HttpResponse('Hello from App!')
 
 
 def db(request):
