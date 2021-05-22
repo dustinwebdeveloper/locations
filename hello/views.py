9cde029c04017b58e9f3a16c95f8ae301b9035eb
dustinwebdeveloper <dustinwebdeveloper@gmail.com>
2021-05-22 19:53:17 +0200

    changed message again

--- hello/views.py
+++ hello/views.py
@@ -5,7 +5,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from App!')
+    return HttpResponse('Hello from the Location App!')
 
 
 def db(request):
